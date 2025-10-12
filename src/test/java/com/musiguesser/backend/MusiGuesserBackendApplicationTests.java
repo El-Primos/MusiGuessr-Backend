899bd4214451432e2e986dbe45a5dd53398ad9d7
Kemal Tahir Bıcılıoğlu <sskemalss99@gmail.com>
2025-10-12 18:06:23 +0300

    initial commit

---
+++ src/test/java/com/musiguesser/backend/MusiGuesserBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musiguesser.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiGuesserBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
