1a08ba97e5f7df44f5defe253e98a562e5d63014
Kaan Karataş <karataskn20@gmail.com>
2025-12-19 01:21:59 +0300

    Add playlist repositories

---
+++ src/main/java/com/musiguessr/backend/repository/PlaylistItemRepository.java
@@ -0,0 +1,18 @@
+﻿package com.musiguessr.backend.repository;
+
+import com.musiguessr.backend.model.PlaylistItem;
+import com.musiguessr.backend.model.PlaylistItemId;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaylistItemRepository extends JpaRepository<PlaylistItem, PlaylistItemId> {
+
+    boolean existsByIdPlaylistIdAndIdSongId(Long playlistId, Long songId);
+
+    List<PlaylistItem> findByIdPlaylistIdOrderByPositionAsc(Long playlistId);
+
+    Optional<PlaylistItem> findByIdPlaylistIdAndIdSongId(Long playlistId, Long songId);
+
+    void deleteByIdPlaylistIdAndIdSongId(Long playlistId, Long songId);
+}
