85ce430de043515c081c7fa7bcb5c1a12d854471
Umut Özil <umut373.uz@gmail.com>
2025-12-21 20:56:45 +0300

    add refresh token entities

---
+++ src/main/java/com/musiguessr/backend/model/RefreshToken.java
@@ -0,0 +1,30 @@
+package com.musiguessr.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "refresh_tokens")
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "fk_refresh_token_user"))
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(name = "expiry_date", nullable = false)
+    private Instant expiryDate;
+}
